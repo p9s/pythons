157990711d72e78c38fabf9c2e9afe191e062ec9
p9s <mc.cheung@aol.com>
2017-06-07 16:14:47 +0800

    demo

---
+++ 糗事百科.py
@@ -0,0 +1,40 @@
+import urllib.request
+import re
+
+def getcontent(url, page):
+    headers = ( "User-Agent", "Mozilla/5.0" )
+    opener = urllib.request.build_opener()
+    opener.addheaders = [headers]
+
+    urllib.request.install_opener( opener )
+
+    data = urllib.request.urlopen(url).read().decode("utf-8")
+
+    userpat = 'target="_blank" title="(.*?)"'
+    contentpat = '<div class="content">(.*?)</div>'
+    
+    userlist = re.compile( userpat, re.S ).findall(data)
+    contentlist = re.compile( contentpat, re.S ).findall(data)
+
+    x = 1
+    for content in contentlist:
+        content = content.replace("\n", '')
+        name = "content" + str(x)
+        exec( name+'=content' )
+        x+=1
+
+    y = 1
+    for user in userlist:
+        name="content"+str(y)
+        print("用户 " + str(page) + str(y) + " 是: " + user )
+        print( "内容是: ")
+        exec("print(" + name + ")")
+        print( "\n")
+        y+=1
+
+
+for i in range( 1, 30 ):
+    url = "http://www.qiushibaike.com/8hr/page/" + str( i )
+    getcontent(url, i )
+
+
